0735324d23eccc64278b2331dd3d5955e9bf2129
Livid <v2ex.livid@mac.com>
2010-10-08 01:03:52 +0800

    You can test some zh-hans locale now.

--- v2ex/babel/l10n/messages/zhHans.py
+++ v2ex/babel/l10n/messages/zhHans.py
@@ -12,10 +12,137 @@ signin = '登入'
 signup = '注册'
 signout = '登出'
 
+# Messages shared by forms
+
+chevron = '<span class="chevron">&nbsp;›&nbsp;</span>'
+resolve_errors = "请解决以下错误之后再继续"
+failed_signin = "用户名/密码不匹配"
+delete = '删除'
+submit = '提交'
+save = '保存'
+create = '创建'
+back_to = '返回'
+yes = '是'
+no = '否'
+
+# Messages on /signin
+
+password = "密码"
+remember_1_year = "登录后 cookie 会被记住一年"
+forgot_password = "我忘记密码了"
+existing_member = '已注册用户请 <a href="/signin">登入</a>'
+
+# Messages on /signup
+
+username_empty = '请输入用户名'
+username_too_long = '用户名长度不能超过 32 个字符'
+username_invalid = '用户名只能使用 a-z, A-Z 及 _'
+username_taken = '抱歉，这个用户名已经被注册了'
+
+# Messages on /signout
+
+signin_again = '重新登入'
+
 # Messages on /
+
+signup_now = "现在注册"
+hottest_nodes = '最热节点'
+new_nodes = '最近新增节点'
+community_stats = '社区运行状态'
+total_members = '注册会员总数'
+total_topics = '主题总数'
+total_replies = '回复总数'
+recent_visited_nodes = '最近访问节点'
+rss_subscribe = 'RSS 订阅社区最新更新'
+more_recent_topics = '更多新主题'
+nodes_portal = '节点导航'
 
 # Messages on /t/
 
+no_reply = '目前尚无回复'
+add_a_reply_now = '现在添加一条回复'
+send = '发送'
+better_to_block = "与其与人争吵，不如将其屏蔽。"
+reply_content_cannot_be_empty = "回复内容不能为空"
+newer_replies_first = "新回复在前"
+earlier_replies_first = "按回复时间排序"
+show_all = "显示全部"
+just_topic_creator = "只看楼主"
+replies = "回复"
+till = "直到"
+sure_to_delete = "确认删除？"
+
 # Messages on /go/
 
+create_new_topic = "创建新主题"
+next = "下一页"
+previous = "上一页"
+header_replies = "回复"
+header_subject = "标题"
+header_author = "作者"
+header_last_replied = "最后回复时间"
+
 # Messages on /settings
+
+username = "用户名"
+email = "电子邮件"
+website = "个人网站"
+twitter = "Twitter"
+location = "所在地"
+tagline = "签名"
+bio = "个人简介"
+language = "语言"
+twitter_sync = "Twitter 同步"
+save_settings = "保存设置"
+avatar = "头像"
+current_avatar = "当前头像"
+upload_new_avatar = "上传新头像"
+gravatar_support = '本站亦支持　<a href="http://www.gravatar.com/" target="_blank">Gravatar</a>'
+security = "安全"
+leave_blank = "如果你不想更改密码，请留空以下输入框。"
+current_password = "当前密码"
+new_password = "新密码"
+change_password = "更改密码"
+account_status = "帐号状态"
+twitter_not_linked = "Twitter 帐号尚未绑定"
+twitter_link_explained = "通过 OAuth 可以安全地绑定你的 Twitter 账户，之后即可同步你的主题和回复。"
+personal_records = "个人纪录"
+blocked = "屏蔽"
+updated_every_4_hours = "以上数据大约每 4 小时更新一次"
+
+# Messages on /member/
+
+recent_topics = '最近创建主题'
+recent_participated_topics = '最近参与主题'
+
+# Messages on /backstage
+
+site_settings = '站点设置'
+site_settings_explained = '站点名，标语，描述及其他更多细节。'
+topic_settings = '主题设置'
+topic_settings_explained = '配置主题类型及谁可以创建主题，谁可以回复。'
+sections = '区域'
+add_new_section = '添加新区域'
+minisites = '迷你站'
+add_new_minisite = '添加新迷你站'
+data_management = '数据管理'
+delete_specified_cached_item = '删除指定的缓存项目'
+delete_specified_cached_item_explained = '你可以在这里输入指定的缓存项目的名字并删除它。'
+mapreduce_explained = 'MapReduce 是一个用于数据整理的超强工具，请谨慎使用。'
+latest_joined = '最新加入会员'
+
+# Messages on /backstage/site
+
+site_name = '站点名'
+site_slogan = '站点标语'
+site_domain = '站点主域名'
+site_description = '站点描述'
+site_home_categories = '主页分类'
+one_category_per_line = '每行一个'
+site_settings_updated = '站点设置成功更新'
+
+# Messages on /backstage/topic
+
+enable_topic_types = '开启主题类型'
+topic_types = '主题类型'
+topic_types_explained = '每行一个，使用 : 分割名字和颜色'
