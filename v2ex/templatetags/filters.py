367a206fae7620bc0409b7babafa28ba4d2d367b
Livid <v2ex.livid@mac.com>
2010-11-14 18:47:47 +0800

    cl.ly is fixed.

--- v2ex/templatetags/filters.py
+++ v2ex/templatetags/filters.py
@@ -32,7 +32,7 @@ def clly(value):
         for img in imgs:
             img_id = re.findall('http://cl.ly/([a-zA-Z0-9]+)', img)
             if (img_id[0] != 'demo' and img_id[0] != 'whatever'):
-                value = value.replace('http://cl.ly/' + img_id[0], '<a href="http://cl.ly/' + img_id[0] + '" target="_blank"><img src="http://cl.ly/' + img_id[0] + '/content" class="imgly" border="0" /></a>')
+                value = value.replace('http://cl.ly/' + img_id[0], '<a href="http://cl.ly/' + img_id[0] + '" target="_blank"><img src="http://zdxproxy.appspot.com/cl.ly/' + img_id[0] + '/content" class="imgly" border="0" /></a>')
         return value
     else:
         return value
